32273d8c45ea1a911564244da534b2c730ed7254
Gabriel Montemor <bielmontemor7@gmail.com>
2026-09-07 12:42:20 -0300

    Adiciona exemplo de Records em Java

---
+++ Programando o Mundo Real com Orientação a Objetos em Java/Java e a Arte da Abstração com Classes e Encapsulamento/Records/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("João", 25);
+        System.out.println(person.name());
+        System.out.println(person.age());
+    }
+}
